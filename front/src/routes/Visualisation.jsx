b9748858669e1989a595cd4153f33afeb5f29f58
Prithvi Reddy <u5021997@anu.edu.au>
2018-09-09 06:08:32 +1000

    Frontend complete

--- front/src/routes/Visualisation.jsx
+++ front/src/routes/Visualisation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {inject, observer} from 'mobx-react'
 import Maps from '../components/Maps'
 import {
     Grid,
@@ -7,10 +8,13 @@ import {
 } from 'semantic-ui-react'
 
 
-export default class Page extends Component {
+@inject('UIState')
+@observer
+class Page extends Component {
     render() {
         return (
             <Container fluid  style={{padding:'50px 50px'}}>
+                <h1> Machine Learning Analytics</h1>
                 <Segment>
                     <h2>Aggregate Statics</h2>
                     <Grid columns='equal'>
@@ -27,21 +31,23 @@ export default class Page extends Component {
                 </Segment>
 
                 <Grid columns='equal'>
-                        <Grid.Column>
-                    <Segment> 
-                        <h2>Tax Center Effectiveness By POA</h2>
-                        <Container fluid style={{ height:'800px'}}>
-                            <Maps/>
-                        </Container>
-                    </Segment>
-                        </Grid.Column>
-                        <Grid.Column>
-                    <Segment>
-                        <h2>Individual POA Statistics</h2>
-                    </Segment>
-                        </Grid.Column>
+                    <Grid.Column>
+                        <Segment> 
+                            <h2>Tax Center Effectiveness By POA</h2>
+                            <Container fluid style={{ height:'800px'}}>
+                                <Maps/>
+                            </Container>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Segment>
+                            <h2>Individual POA Statistics</h2>
+                            {this.props.UIState.regionSelected}
+                        </Segment>
+                    </Grid.Column>
                 </Grid>
             </Container>
         )
     }
 }
+export default Page
